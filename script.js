b45beb0d82e6828e413c23d1df0197980e6919e7
jean1902 <cin71781@gmail.com>
2022-01-13 15:58:07 +0100

    dernier commit

--- script.js
+++ script.js
@@ -2,68 +2,65 @@ let formulaire = document.querySelector(".formulaire");
 let btn_search = document.querySelector(".btn_search");
 let input_search = document.querySelector(".inpu_search");
 let api_key = "3cfc3b82d5mshf5c1f17e17c77e1p145e32jsn31efcadf502d";
-
+let dico = document.querySelector(".dico");
 
 //cacher le formulaire au click
 
-fetch(`https://dicolink.p.rapidapi.com/mot/cheval/definitions?rapidapi-key=${api_key}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
+fetch(
+  `https://dicolink.p.rapidapi.com/mot/cheval/definitions?rapidapi-key=${api_key}`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].definition);
 
-//to retrieve Api Data
-
-formulaire.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(input_search.value);
-  if (input_search.value) {
-    fetch(
-      `https://dicolink.p.rapidapi.com/mot/cheval/definitions?rapidapi-key=${api_key}&query=${input_search.value}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        displayDefinition(data);
-      });
-  }
-});
-
-formulaire.addEventListener("keyup", (e) => {
-    e.preventDefault();
-    console.log(input_search.value);
-    if (input_search.value) {
-      fetch(
-        `https://dicolink.p.rapidapi.com/mot/cheval/definitions?rapidapi-key=${api_key}&query=${input_search.value}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          displayDefinition(data);
-        });
-    } else{
-        alert('entrer une recherche valide')
+          dico.innerHTML += `
+          <div class="resutat_search">
+          <div class="head_result">
+          ${data[i].mot}
+      </div>
+      <div class="content_definition">
+          ${data[i].definition}
+      </div>
+      <span> source: ${data[i].source}</span>
+      <span> nature: ${data[i].nature}</span>
+          </div>
+          `;
     }
-  });
-  
 
-function displayDefinition(data) {
-  data.forEach((item) => {
-    formulaire.innerHTML = `
-            <div class="resutat_search">
-         <div class="head_result">
-         <a href="">retour</a>
-         </div>
-         <div class="content_definition">
-             <p class="text_resultat">
-                <h2 class="head_resultat"> ${item.mot}</h3>
-             <p class="text_result">
-             ${item.definition}
-             </p>
-             <span> source: ${item.source}</span>
-             <p> nature: ${item.nature}</span>
-             </p>
-         </div>
-       </div>`;
+    // dico.innerHTML +=
+    // `
+    // <div class="resutat_search">
+    //   <div class="head_result">
+    //   <a href="">retour</a>
+    //   </div>
+    //   <div class="content_definition">
+    //       <p class="text_resultat">
+    //          <h2 class="head_resultat"> ${data.mot}</h3>
+    //       <p class="text_result">
+    //       ${data.definition}
+    //       </p>
+    //       <span> source: ${data.source}</span>
+    //       <p> nature: ${data.nature}</span>
+    //       </p>
+    //   </div>
+    //  </div>
+    // `
   });
-}
+//     formulaire.innerHTML += `
+//     <div class="resutat_search">
+//  <div class="head_result">
+//  <a href="">retour</a>
+//  </div>
+//  <div class="content_definition">
+//      <p class="text_resultat">
+//         <h2 class="head_resultat"> ${item.mot}</h3>
+//      <p class="text_result">
+//      ${item.definition}
+//      </p>
+//      <span> source: ${item.source}</span>
+//      <p> nature: ${item.nature}</span>
+//      </p>
+//  </div>
+// </div>`
